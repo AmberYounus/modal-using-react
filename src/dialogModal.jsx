128659790da2d196bc16688e1d1fc36af1b4fb96
Amber Younus <amberyounus22@gmail.com>
2024-02-21 23:09:17 -0800

    commit

--- src/dialogModal.jsx
+++ src/dialogModal.jsx
@@ -1,14 +1,29 @@
 import React, { useEffect, useRef } from "react";
 
-export function DialogModal(isOpen) {
+export function DialogModal(isOpen,onClose) {
   const dialogRef = useRef(null);
+
   useEffect(() => {
     const dialog = dialogRef.current;
-    if (dialog == null) return;
+
+    if (dialog == null) 
+    return;
     if (isOpen) {
       dialog.showModal();
     } else {
       dialog.close();
     }
   }, [isOpen]);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+
+    if (dialog == null)
+     return;
+    dialog.addEventListener("close", onClose);
+    return () => {
+      dialog.removeEventListener("close", onClose);
+    };
+    
+  }, [onClose]);
 }
